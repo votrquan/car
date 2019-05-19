8e96243f5e1bc2f8f36d84dd4f54b50554052c0b
votrquan <votrquan@gmail.com>
2019-05-20 00:20:41 +0700

    2nd

--- carProject/src/main/java/com/car/controler/mainControler.java
+++ carProject/src/main/java/com/car/controler/mainControler.java
@@ -1,17 +1,39 @@
 package com.car.controler;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.car.model.user;
 
 @Controller
 public class mainControler {
 	
 	@RequestMapping("/")
-	  public String welcome() {
+	  public String welcome(Model model) {
+		model.addAttribute("user", new user()); 
 	    return "index";
 	  }
-	@RequestMapping(value = {"/test" })
-    public String index() {
-        return "test";
+	@RequestMapping(value = {"/test2" })
+    public String index(Model model) {
+		model.addAttribute("user", new user()); 
+        return "test2";
     }
+	
+	@PostMapping("/user/submit")
+	public String save(@Valid user user, BindingResult result, RedirectAttributes redirect) {
+		if (result.hasErrors()) {
+			return "form";
+		}
+		user rs = new user();
+		rs= user;
+		
+		redirect.addFlashAttribute("success", rs.getPhoneNumer());
+		return "test";
+	}
 }
